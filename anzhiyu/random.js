a410f1978836a033989fcee03911fa4b74105a4b
lwbupt <lwbupt@126.com>
2024-12-31 13:55:29 +0800

    Site updated: 2024-12-31 13:55:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/31/hello-world/"];function toRandomPost(){
+var posts=["2024/12/31/一篇新博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
